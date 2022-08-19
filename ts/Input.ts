56f7fffab8804073c8cb4e2d9970afc88251d9f8
jibon <atikurrahaman386@gmail.com>
2022-08-19 18:00:30 +0600

    added touch screen support

--- ts/Input.ts
+++ ts/Input.ts
@@ -1,4 +1,5 @@
 import { Game } from "./Game.js";
+import { Vector2 } from "./Vector2.js";
 
 export class Input {
     keys: Set<string> = new Set();
@@ -8,6 +9,54 @@ export class Input {
         }
         document.onkeyup = (e) => {
             this.keys.delete(e.key);
+        }
+
+        //if it has a touch screen, add touch events
+        if (('ontouchstart' in window) || (navigator.maxTouchPoints > 0)) {
+            let started = false;
+            let started_touch = new Vector2(0, 0);
+            this.game.ctx.canvas.ontouchstart = (e) => {
+                started = true;
+                started_touch = new Vector2(e.touches[0].clientX, e.touches[0].clientY);
+                if (['paused', 'game over'].includes(game.state)) {
+                    this.keys.add(' ');
+                } else {
+                    const canvas_top = this.game.ctx.canvas.getBoundingClientRect().top;
+                    const canvas_left = this.game.ctx.canvas.getBoundingClientRect().left;
+
+                    if (
+                        e.touches[0].clientY - canvas_top >= 10 &&
+                        e.touches[0].clientY - canvas_top <= 16 + 16 &&
+                        e.touches[0].clientX - canvas_left >= 10 &&
+                        e.touches[0].clientX - canvas_left <= 10 + 'Pause'.length * 16
+                    ) {
+                        this.keys.add(' ');
+                    }
+                }
+            }
+            this.game.ctx.canvas.ontouchmove = (e) => {
+                if (started) {
+                    //up
+                    if (e.touches[0].clientY < started_touch.y && Math.abs(started_touch.x - e.touches[0].clientX) < 50) {
+                        this.keys.add('ArrowUp');
+                    }
+                    // right
+                    else if (e.touches[0].clientX > started_touch.x && Math.abs(started_touch.y - e.touches[0].clientY) < 50) {
+                        this.keys.add('ArrowRight');
+                    }
+                    // down
+                    else if (e.touches[0].clientY > started_touch.y && Math.abs(started_touch.x - e.touches[0].clientX) < 50) {
+                        this.keys.add('ArrowDown');
+                    }
+                    // left
+                    else if (e.touches[0].clientX < started_touch.x && Math.abs(started_touch.y - e.touches[0].clientY) < 50) {
+                        this.keys.add('ArrowLeft');
+                    }
+                }
+            }
+            this.game.ctx.canvas.ontouchend = (e) => {
+                started = false;
+            }
         }
     }
 
--- ts/UI.ts
+++ ts/UI.ts
@@ -7,16 +7,30 @@ export default class UI implements Entity {
     velocity!: Vector2;
     width!: number;
     height!: number;
+    touch_screen: boolean = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0);
     draw(): void {
+        this.draw_score();
+
+        this.draw_state();
+        //pause icon
+        this.draw_pause_button(10, 10, 'Pause');
+
+
+
+    }
+    draw_pause_button(x: number, y: number, text: string) {
+        if(this.game.state === 'playing' && this.touch_screen){
         this.game.ctx.fillStyle = 'white';
-        //font size 16px
+        this.game.ctx.fillRect(x, y, text.length * 16, 16 + 16);
+        this.game.ctx.fillStyle = 'black';
+        //align: center
+        this.game.ctx.textAlign = 'center';
+        //fz16
         this.game.ctx.font = '16px Arial';
-        //text align right
-        this.game.ctx.textAlign = 'right';
-        //text baseline top
-        this.game.ctx.textBaseline = 'top';
-        this.game.ctx.fillText(`Score: ${this.game.score.score}`, this.game.ctx.canvas.width - 10, 10);
-        this.game.ctx.fillText(`High Score: ${this.game.score.high_score}`, this.game.ctx.canvas.width - 10, 32);
+        this.game.ctx.fillText(text, (x + text.length * 16) / 2, y + 8);
+        }
+    }
+    draw_state() {
         if (this.game.state !== 'playing') {
             this.game.ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
             this.game.ctx.fillRect(0, 0, this.game.ctx.canvas.width, this.game.ctx.canvas.height);
@@ -27,12 +41,23 @@ export default class UI implements Entity {
             this.game.ctx.textAlign = 'center';
 
             if (this.game.state == 'paused') {
-                this.game.ctx.fillText(`Paused, press 'Space' to play.`, this.game.ctx.canvas.width / 2, this.game.ctx.canvas.height / 2);
+                this.game.ctx.fillText(`Paused, ${this.touch_screen ? `'Tap'` : `press 'Space'`} to play.`, this.game.ctx.canvas.width / 2, this.game.ctx.canvas.height / 2);
             } else {
                 this.game.ctx.fillText(`Game Over, press 'Space' to play again.`, this.game.ctx.canvas.width / 2, this.game.ctx.canvas.height / 2);
             }
-
         }
+    }
+    draw_score() {
+        //color white
+        this.game.ctx.fillStyle = 'white';
+        //fz16
+        this.game.ctx.font = '16px Arial';
+        //text-align: right
+        this.game.ctx.textAlign = 'right';
+        //text-baseline: top
+        this.game.ctx.textBaseline = 'top';
+        this.game.ctx.fillText(`Score: ${this.game.score.score}`, this.game.ctx.canvas.width - 10, 10);
+        this.game.ctx.fillText(`High Score: ${this.game.score.high_score}`, this.game.ctx.canvas.width - 10, 32);
     }
     update(deltaTime: number): void {
     }
